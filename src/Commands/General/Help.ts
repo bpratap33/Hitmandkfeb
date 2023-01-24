a84396923de390249de9aebd314192b83d92011a
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-01-24 07:20:39 +0200

    Update Help.ts

--- src/Commands/General/Help.ts
+++ src/Commands/General/Help.ts
@@ -31,7 +31,7 @@ export default class extends BaseCommand {
             for (const category of categories) {
                 const categoryCommands: string[] = []
                 const filteredCommands = commands.filter((command) => command.data.config.category === category)
-                text += `\n\n*╚━☾︎🌹 ${this.client.utils.capitalize(category)} 🌹☽︎━╝*\n\n`
+                text += `\n\n*╚━☾︎😻 ${this.client.utils.capitalize(category)} 😻☽︎━╝*\n\n`
                 filteredCommands.forEach((command) => categoryCommands.push(command.data.name))
                 text += `\`\`\`${categoryCommands.join(', ')}\`\`\``
             }
