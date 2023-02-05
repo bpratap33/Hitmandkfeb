f4fa08038a5cc752dc80caba493b4850f42b06ce
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-02-05 14:39:48 +0200

    Create Invitelink.ts

---
+++ src/Commands/General/Invitelink.ts
@@ -0,0 +1,27 @@
+import { BaseCommand, Command, Message } from '../../Structures'
+import { AnyMessageContent } from '@adiwajshing/baileys'
+
+@Command('invitelink', {
+    description: 'invitelink',
+    category: 'general',
+    usage: 'invitelink',
+    cooldown: 5,
+    exp: 10,
+    dm: true
+})
+export default class command extends BaseCommand {
+    override execute = async (M: Message): Promise<void> => {
+        let text = ''
+        const { invitelink } = this.client.config
+        for (let i = 0; i < invitelink.length; i++) {
+            const { subject } = await this.client.groupMetadata(M.from[invitelink])
+            const code = await this.client.groupInviteCode(M.from[invitelink])
+            text += `\n*#${i + 1}*\n*${subject}:* *https://chat.whatsapp.com/${code}*\n`
+            if (!invitelink) *invitelink* = `https://chat.whatsapp.com/${code}`
+        }
+        await this.client.sendMessage(M.sender.jid, {
+            text
+        } as unknown as AnyMessageContent)
+        return void M.reply('Sent the group links in your DM')
+    }
+}
