ec1970ea6ad0cd8ba0e8dc595f878e4ee7153dfd
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-02-05 15:27:49 +0200

    Update Config.ts

--- src/Types/Config.ts
+++ src/Types/Config.ts
@@ -9,6 +9,8 @@ export interface IConfig {
     chatBotUrl: string
     /**number of the users who's the bot admins of the bot */
     mods: string[]
+    /** JIDS of the support groups */
+    supportGroups: string[]
     /**port number where the server will be started */
     PORT: number
 }
