8e6859050d01f5a246bddc01e6085beff4029370
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-02-06 15:46:39 +0200

    Update User.ts

--- src/Database/Models/User.ts
+++ src/Database/Models/User.ts
@@ -16,6 +16,9 @@ export class UserSchema {
 
     @prop({ type: String, required: true })
     public tag!: string
+
+    @prop({ type: Number, required: true, default: 0 })
+    public lastDaily!: number
 }
 
 export type TUserModel = UserSchema & Document
