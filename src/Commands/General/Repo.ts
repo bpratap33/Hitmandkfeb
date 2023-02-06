35dab0e891ed1307ab7b7cb6473494ad116951a9
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-02-06 17:31:37 +0200

    Update Repo.ts

--- src/Commands/General/Repo.ts
+++ src/Commands/General/Repo.ts
@@ -42,7 +42,7 @@ interface RepoInfo {
     watchers_count: number
     forks_count: number
     Followers: number
-    Repository: name
+    Repository: number
     open_issues_count: number
     visibility: string
     license: {
